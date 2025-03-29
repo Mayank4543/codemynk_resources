b35187b16bc7f233a59365e93f17adc5c8ce0de0
= <mayankrathore9897@gmail.com>
2025-03-29 22:24:37 +0530

    Adding notes

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -12,8 +12,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
-
-
 const jobs = [
   {
     position: "Resume",
